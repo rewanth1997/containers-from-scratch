4cab4ab4e3c2f5a8d83578e3af27d033c3c52268
Rewanth Cool <rewanth1997@gmail.com>
2020-10-31 18:11:25 +0000

    Adds code to run rootless container

--- README.md
+++ README.md
@@ -1,6 +1,32 @@
 # containers-from-scratch
-Writing a container in a few lines of Go code, as seen at [DockerCon 2017](https://www.youtube.com/watch?v=MHv6cWjvQjM&t=1316s) and on [O'Reilly Safari](https://www.safaribooksonline.com/library/view/how-to-containerize/9781491982310/)
 
-You need root permissions for this version to work. Or you can adapt it to be a rootless container by as shown in [these slides](https://speakerdeck.com/lizrice/rootless-containers-from-scratch). 
+## Motivation
 
-Note that the Go code uses some syscall definitions that are only available when building with GOOS=linux.
+At the time of forking, Liz's containers-from-scratch project starts container with root privileges. That means, any process that is running with root privielges inside the container also runs with root privileges on host machine. This kind of behaviour causes a lot of security concerns.
+
+This updated project contains code that allows you to start **"rootless container"** with just a few linux syscalls.
+
+*Note that the Go code uses some syscall definitions that are only available when building with GOOS=linux.*
+
+## Dependency
+
+To start a containerized environment, you need a filesystem to mount. We will mount alpine filesystem to start our containerized environment. You can use any filesystem for that matter.
+
+*Note: Alpine filesystem is already included in the project*
+
+Download reference: http://dl-cdn.alpinelinux.org/alpine/v3.12/releases/x86_64/alpine-minirootfs-3.12.1-x86_64.tar.gz
+
+## Execution
+
+```
+go run main.go run /bin/sh
+```
+
+## Rootless container in action
+
+![Rootless container in action](./rootless-container-poc.png)
+
+
+## Contributor
+
+[Rewanth Cool](https://www.linkedin.com/in/rewanthcool/)
--- main.go
+++ main.go
@@ -2,45 +2,48 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
-	"os/exec"
 	"path/filepath"
+	"io/ioutil"
 	"strconv"
 	"syscall"
+	"os/exec"
 )
 
-// go run main.go run <cmd> <args>
 func main() {
 	switch os.Args[1] {
-	case "run":
-		run()
-	case "child":
-		child()
-	default:
-		panic("help")
+		case "run":
+			run()
+		case "child":
+			child()
+		default:
+			panic("no specified args found")
 	}
 }
 
 func run() {
-	fmt.Printf("Running %v \n", os.Args[2:])
+	fmt.Printf("Running host process %v as pid %d\n", os.Args[2:], os.Getpid())
 
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags:   syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
-		Unshareflags: syscall.CLONE_NEWNS,
+		Cloneflags:   syscall.CLONE_NEWUTS|syscall.CLONE_NEWPID|syscall.CLONE_NEWNS|syscall.CLONE_NEWUSER,
+		Credential: &syscall.Credential{Uid: 0, Gid: 0},
+		UidMappings: []syscall.SysProcIDMap {
+			{ContainerID: 0, HostID: os.Getuid(), Size: 1},
+		},
+		GidMappings: []syscall.SysProcIDMap{
+			{ContainerID: 0, HostID: os.Getuid(), Size: 1},
+		},
 	}
 
 	must(cmd.Run())
 }
 
 func child() {
-	fmt.Printf("Running %v \n", os.Args[2:])
-
-	cg()
+	fmt.Printf("Running child process %v as pid %d\n", os.Args[2:], os.Getpid())
 
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
@@ -48,25 +51,24 @@ func child() {
 	cmd.Stderr = os.Stderr
 
 	must(syscall.Sethostname([]byte("container")))
-	must(syscall.Chroot("/home/liz/ubuntufs"))
-	must(os.Chdir("/"))
+	must(syscall.Chroot("./alpinefs"))
+	must(syscall.Chdir("/"))
 	must(syscall.Mount("proc", "proc", "proc", 0, ""))
-	must(syscall.Mount("thing", "mytemp", "tmpfs", 0, ""))
 
 	must(cmd.Run())
 
 	must(syscall.Unmount("proc", 0))
-	must(syscall.Unmount("thing", 0))
 }
 
 func cg() {
 	cgroups := "/sys/fs/cgroup/"
 	pids := filepath.Join(cgroups, "pids")
-	os.Mkdir(filepath.Join(pids, "liz"), 0755)
-	must(ioutil.WriteFile(filepath.Join(pids, "liz/pids.max"), []byte("20"), 0700))
-	// Removes the new cgroup in place after the container exits
-	must(ioutil.WriteFile(filepath.Join(pids, "liz/notify_on_release"), []byte("1"), 0700))
-	must(ioutil.WriteFile(filepath.Join(pids, "liz/cgroup.procs"), []byte(strconv.Itoa(os.Getpid())), 0700))
+
+	must(os.Mkdir(filepath.Join(pids, "ubuntu"), 0755))
+	must(ioutil.WriteFile(filepath.Join(pids, "ubuntu/pids.max"), []byte("20"), 0700))
+	// Removes the new cgroup in place
+	must(ioutil.WriteFile(filepath.Join(pids, "ubuntu/notify_on_release"), []byte("1"), 0700))
+	must(ioutil.WriteFile(filepath.Join(pids, "ubuntu/cgroup.procs"), []byte(strconv.Itoa(os.Getpid())), 0700))
 }
 
 func must(err error) {
